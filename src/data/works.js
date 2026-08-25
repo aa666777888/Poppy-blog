a3de0a974c08d0949659a0adb3c084b9dfabdc0b
aa666777888 <aa2025113369@gmail.com>
2026-08-25 11:08:22 +0800

    Change status to Ongoing

--- src/data/works.js
+++ src/data/works.js
@@ -7,7 +7,7 @@ const works = [
     id: 1,
     title: 'Savior Complex',
     type: 'Novel',
-    status: '连载中',
+    status: 'Ongoing',
     cover: 'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSI2MDAiIGhlaWdodD0iODAwIj48cmVjdCB3aWR0aD0iNjAwIiBoZWlnaHQ9IjgwMCIgZmlsbD0iIzNCMUU1RSIvPjwvc3ZnPg==',
     description: 'Harry Potter keeps noticing the bruises hidden under Draco Malfoy’s sleeves — and can’t stop himself from trying to help, even if it means pretending to be a girl with a crush.',
     date: '2026-08-25',
